80e828d677617a0ab45fd6115efa957fa07bbb4b
Yaseeru <asulaiman1411@gmail.com>
2025-05-14 23:25:50 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{html,js}",
+    "./src/**/*.{vue,js}",
+    "./src/**/*.{svelte,js}",
+    "./src/**/*.{astro,js}",
+    "./src/**/*.{mdx,js}",
+    "./src/**/*.{css,js}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
